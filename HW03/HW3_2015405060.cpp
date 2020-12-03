4ab0300f3cf4c2abdfff0954fe76b6cb02ac3b5d
On <62434898+cks3066@users.noreply.github.com>
2020-12-04 03:18:47 +0900

    Add files via upload

---
+++ HW03/HW3_2015405060.cpp
@@ -0,0 +1,36 @@
+/*2015405060 ¿Â½ÂÂù*/
+/*Visual Studio 2019*/
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int N, max = 0;
+	string str;
+
+	unordered_map<string, int> wordFreq;
+	cin >> N;
+	cin >> str;
+
+	string word;
+	for (int i = 0; i < (int)str.length() - N; i++)
+	{
+		word = str.substr(i, N);
+		wordFreq[word]++;
+	}
+	unordered_map<string, int>::iterator p;
+	for (p = wordFreq.begin(); p != wordFreq.end(); p++)
+	{
+		if (p->second >= max) {
+			max = p->second;
+			word = p->first;
+		}
+	}
+	cout << word << endl;
+
+	return 0;
+}
